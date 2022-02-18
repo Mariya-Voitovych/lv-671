f2d3e0953517af152f5f627fe3170f5a4abaf7ea
savka-olia <tokarchykolia@gmail.com>
2022-02-18 14:33:35 +0200

    tasks were added

---
+++ lib/tasks/savka/savka_7.rb
@@ -0,0 +1,13 @@
+puts "This program returns an array containing repetitions of the number argument.\n\n"
+
+def replicate(times, number)
+  arr = []
+  if times < 1
+    arr
+  else
+    arr.append(number)
+    arr.concat(replicate(times - 1, number))
+  end
+end
+
+replicate(5, 3)
