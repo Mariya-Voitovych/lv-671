45882f56cc97de86092f60497cbcf5f778490a4a
MariyaVoitovych <urkevych@gmail.com>
2022-02-18 11:12:56 +0200

    Added comments to five kata

--- lib/tasks/voitovych_5.rb
+++ lib/tasks/voitovych_5.rb
@@ -13,15 +13,16 @@ def closest(strng)
   weigth = weigth(numbers)
   distances = find_dist(weigth)
   min_distance = find_min(distances)
-  if min_distance.length == 1
+  if min_distance.length == 1                                            #the first condition is satisfied by one pair of numbers
     form_answer(min_distance, numbers)
-  elsif smallest_weigth(min_distance, weigth).length == 1
+  elsif smallest_weigth(min_distance, weigth).length == 1                #the second condition is satisfied by one pair of numbers
     form_answer(smallest_weigth(min_distance, weigth), numbers)
   else
     form_answer(smallest_index(smallest_weigth(min_distance, weigth)), numbers)
   end
 end
 
+#returns an array of weigths number
 def weigth(numbers)
   weigth = []
   (0..numbers.length - 1).each do |i|
@@ -32,6 +33,7 @@ def weigth(numbers)
   weigth
 end
 
+#returns hash where keys are indices two numbers and value is distance between them  
 def find_dist(array)
   dist = {}
   (0..array.length - 1).each do |i|
@@ -42,11 +44,27 @@ def find_dist(array)
   dist
 end
 
+#finds all numbers witn min distance
 def find_min(hash)
   min = hash.values.min
   hash.select { |_key, value| value == min }
 end
 
+#finds all numbers witn min sum of weigths
+def smallest_weigth(hash, weigth)
+  weigths = []
+  hash.each_key { |key| weigths.push(weigth[key[0]] + weigth[key[1]]) }
+  hash.select { |key, _value| weigth[key[0]] + weigth[key[1]] == weigths.min }
+end
+
+#finds two numbers witn the smallest index
+def smallest_index(hash)
+  index = []
+  hash.keys.each { |key| index.push(key[0]) }
+  hash.select { |key, _value| key[0] == index.min }
+end
+
+#forms the answer according to requirements 
 def form_answer(hash, numbers)
   answer = []
   (0..1).each do |i|
@@ -54,18 +72,6 @@ def form_answer(hash, numbers)
     answer.push([weigth(numbers)[index], index, numbers[index].to_i])
   end
   answer.sort_by { |arr| arr[0] }
-end
-
-def smallest_weigth(hash, weigth)
-  weigths = []
-  hash.each_key { |key| weigths.push(weigth[key[0]] + weigth[key[1]]) }
-  hash.select { |key, _value| weigth[key[0]] + weigth[key[1]] == weigths.min }
-end
-
-def smallest_index(hash)
-  index = []
-  hash.keys.each { |key| index.push(key[0]) }
-  hash.select { |key, _value| key[0] == index.min }
 end
 
 strng = "456899 50 11992 176 272293 163 389128 96 290193 85 52"
