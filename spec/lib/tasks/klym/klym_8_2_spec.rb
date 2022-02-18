6b863c468cbf704939c05a8a2c9948c75fb47709
olehkovalchuk <rod07@ukr.net>
2022-02-18 14:40:41 +0200

    add tests

---
+++ spec/lib/tasks/klym/klym_8_2_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe 'litres' do
+  subject { litres(time) }
+  context 'when argument is correct' do
+    let(:time) { 10 }
+    it 'should return correct value' do
+      expect(subject).to eq(5)
+    end
+  end
+  context 'when argument is uncorrect - string' do
+    let(:time) { 'string' }
+    it 'should raise NoMethodError' do
+      expect { subject }.to raise_error(NoMethodError)
+    end
+  end
+end
