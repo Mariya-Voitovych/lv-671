62fd406793d69d4058335314de29d9d6aa45da09
pomellooo <80164334+pomellooo@users.noreply.github.com>
2022-02-19 11:18:58 +0200

    Delete trynoha_5.rb

--- spec/lib/tasks/trynoha/trynoha_5.rb
+++
@@ -1,8 +0,0 @@
-require './files_to_test/perimetr_in_square_in_a_rectangle'
-
-describe 'summ of perimetrs' do
-  it 'summ of squares' do
-    expect(perimeter(5)).to eql(80)
-    expect(perimeter(7)).to eql(216)
-  end
-end
