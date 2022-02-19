a0e76099da96d561fa71f04dd9d5ff62cda32ec6
Roksoliana <yatsunda7@gmail.com>
2022-02-19 17:43:58 +0200

    removed changes

--- menu_functions.rb
+++ menu_functions.rb
@@ -1,7 +1,5 @@
 require_relative './show'
 module MenuFunctions
-  PATH_TASK = 'c:\Users\Roksa\lv-671\lib\tasks'
-  PATH_TESTS = 'c:\Users\Roksa\lv-671\spec\lib\tasks'
   class Help
     class << self
       def call(input)
@@ -69,13 +67,13 @@ module MenuFunctions
   end
   class AllTests
     def self.call(input)
-      available_dirs = Dir.entries(PATH_TESTS).drop(2)
-      username = available_dirs.map{ |string| string.split("_")[0] }.uniq[0]
-      p username
-      puts "Choose author`s name"
-      input = gets.chomp.downcase
-      allusersfiles = available_dirs.find_all{ |k| k.start_with?(username) }.map{|string| PATH_TESTS + "\\" + "#{string}"}
-      p allusersfiles
+      p available_dirs = Dir.entries(PATH_TESTS).drop(2)
+      # username = available_dirs.map{ |string| string.split("_")[0] }.uniq[0]
+      # p username
+      # puts "Choose author`s name"
+      # input = gets.chomp.downcase
+      # allusersfiles = available_dirs.find_all{ |k| k.start_with?(username) }.map{|string| PATH_TESTS + "\\" + "#{string}"}
+      # p allusersfiles
       # system('rspec', allusersfiles.join(" "))
     end
   end
