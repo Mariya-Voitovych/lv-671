e61b73469b2ff84eee5cad13ca1d78014fc566de
pomellooo <80164334+pomellooo@users.noreply.github.com>
2022-02-19 11:19:22 +0200

    Delete trynoha_8_2.rb

--- spec/lib/tasks/trynoha/trynoha_8_2.rb
+++
@@ -1,9 +0,0 @@
-require './files_to_test/wilson_primes'
-require 'bigdecimal'
-
-describe '.Wilson_primes' do
-  it 'returns 1 if n less 1' do
-    expect(am_I_Wilson(5)).to eql(true)
-    expect(am_I_Wilson(8)).to eql(false)
-  end
-end
