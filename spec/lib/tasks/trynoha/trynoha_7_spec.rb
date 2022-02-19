f76b1e08b1f2c00c3610defb1eabfca3197ebbf9
Taras <pentsko.t@gmail.com>
2022-02-19 11:15:44 +0200

    rename files and delete req_relative

---
+++ spec/lib/tasks/trynoha/trynoha_7_spec.rb
@@ -0,0 +1,8 @@
+describe 'return donation' do
+  it 'return newavg if arr.empty' do
+    expect(new_avg([], 2)).to eql(2)
+  end
+  it 'return newavg if arr.empty' do
+    expect(new_avg([0,0], 2)).to eql(6)
+  end
+end
---
+++ spec/lib/tasks/trynoha/trynoha_8_2_spec.rb
@@ -0,0 +1,8 @@
+require 'bigdecimal'
+
+describe '.Wilson_primes' do
+  it 'returns 1 if n less 1' do
+    expect(am_I_Wilson(5)).to eql(true)
+    expect(am_I_Wilson(8)).to eql(false)
+  end
+end
