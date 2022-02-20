a19b78ef4c1fc76de9421069097c10684a2be56c
Klym Oleh <k4138314@gmail.com>
2022-02-20 13:52:27 +0200

    Update klym_6_spec.rb

--- spec/lib/tasks/klym/klym_6_spec.rb
+++ spec/lib/tasks/klym/klym_6_spec.rb
@@ -5,11 +5,4 @@ describe '#f(x)' do
     expect(f(x)).to eq 1.29999999155e-08
     end
   end
-
-  context 'when passed argument equal 0' do
-    it "returns a good approximation of f(x)" do
-      x = 2.6e-08
-      expect(f(x)).to eq 1.29999999155e-08
-    end  
-  end
 end
