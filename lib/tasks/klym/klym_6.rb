6e30e18ea5b0814e63ce14a77fcbd04cad4ffca2
Klym Oleh <k4138314@gmail.com>
2022-02-22 09:23:40 +0200

    Update klym_6.rb

--- lib/tasks/klym/klym_6.rb
+++ lib/tasks/klym/klym_6.rb
@@ -1,7 +1,8 @@
 puts "This program returns better approximation of f(x) in the neighborhood of 0"
-
-def f(x)
+# comment
+def f(x) #comment
   x.fdiv(1 + Math.sqrt(1 + x))
 end
 
+puts "f(2.6e-08) =>#{f(2.6e-08)}" #comment
 puts "f(2.6e-08) =>#{f(2.6e-08)}"
