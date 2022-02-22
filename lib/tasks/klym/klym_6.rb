dca8e022df5307577a3b99c157078fbd376035e1
Klym Oleh <k4138314@gmail.com>
2022-02-22 09:23:53 +0200

    Revert "Update klym_6.rb"
    This reverts commit 6e30e18ea5b0814e63ce14a77fcbd04cad4ffca2.

--- lib/tasks/klym/klym_6.rb
+++ lib/tasks/klym/klym_6.rb
@@ -1,8 +1,7 @@
 puts "This program returns better approximation of f(x) in the neighborhood of 0"
-# comment
-def f(x) #comment
+
+def f(x)
   x.fdiv(1 + Math.sqrt(1 + x))
 end
 
-puts "f(2.6e-08) =>#{f(2.6e-08)}" #comment
 puts "f(2.6e-08) =>#{f(2.6e-08)}"
