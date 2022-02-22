98f850b034043c338ccbc008a5fd29033ef9ab22
Roksoliana <yatsunda7@gmail.com>
2022-02-22 22:21:51 +0200

    fix my tests (#17)

--- spec/lib/tasks/voitovych/voitovych_5_spec.rb
+++ spec/lib/tasks/voitovych/voitovych_5_spec.rb
@@ -1,8 +1,8 @@
 describe "#closest" do
     it "return closest " do    
-      expect(kata.closest("")).to eq []
+      expect(closest("")).to eq []
     end
     it "return closest" do    
-      expect(kata.closest("456899 50 11992 176 272293 163 389128 96 290193 85 52")).to eq [[13, 9, 85], [14, 3, 176]]
+      expect(closest("456899 50 11992 176 272293 163 389128 96 290193 85 52")).to eq [[13, 9, 85], [14, 3, 176]]
     end
 end
--- spec/lib/tasks/voitovych/voitovych_8_2_spec.rb
+++ spec/lib/tasks/voitovych/voitovych_8_2_spec.rb
@@ -1,8 +1,8 @@
 describe "#circle_area " do
     it "calculates the area of that circle " do   
-      expect(kata.circle_area(Circle.new(Point.new(10, 10), 30)).round(6)).to eq(2827.433388)
+      expect(circle_area(Circle.new(Point.new(10, 10), 30)).round(6)).to eq(2827.433388)
     end
     it "calculates the area of that circle " do   
-      expect(kata.circle_area(Circle.new(Point.new(25, -70), 30)).round(6)).to eq(2827.433388)
+      expect(circle_area(Circle.new(Point.new(25, -70), 30)).round(6)).to eq(2827.433388)
     end
 end
