2bf6f45e35e1e7ebe9760e644bf356eac09447ef
MariyaVoitovych <urkevych@gmail.com>
2022-02-23 11:43:13 +0200

    Changed function name to mant_exp

--- lib/tasks/voitovych/voitovych_6.rb
+++ lib/tasks/voitovych/voitovych_6.rb
@@ -1,4 +1,4 @@
-def mantExp(a_number, digits_number)
+def mant_exp(a_number, digits_number)
   i = 0
   a_number = a_number.to_f
   while a_number.to_i.to_s.length < digits_number
@@ -16,6 +16,6 @@ end
 if $PROGRAM_NAME == __FILE__
   puts"A ﬂoating-point number can be represented as mantissa * radix ^ exponent.
 This program returns argument in the form of a string concatenation of mantissa, P and exponent."
-puts "(0.06, 10) => #{mantExp(0.06, 10)}"
-puts "(123456.0, 4) => #{mantExp(123456.0, 4)}"
+puts "(0.06, 10) => #{mant_exp(0.06, 10)}"
+puts "(123456.0, 4) => #{mant_exp(123456.0, 4)}"
 end
