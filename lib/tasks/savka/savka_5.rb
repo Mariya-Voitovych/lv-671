abeac4b6120a04231b78c65d12eb3740ff8fbabf
savka-olia <tokarchykolia@gmail.com>
2022-03-15 15:54:23 +0200

    add description to the 5 task

--- lib/tasks/savka/savka_5.rb
+++ lib/tasks/savka/savka_5.rb
@@ -10,5 +10,7 @@ def solve(m)
 end
 
 if $PROGRAM_NAME == __FILE__
-p solve(2.0) 
+  puts "Given the sequence U(n, x) = x + 2x**2 + 3x**3 + .. + nx**n where x is a real number and n a positive integer. This sequence
+  is a power series. When we solve this we get a quadratic equation which we can solve with the quadratic formula for the roots of the general quadratic equation.\n\n"
+  p solve(2.0) 
 end
